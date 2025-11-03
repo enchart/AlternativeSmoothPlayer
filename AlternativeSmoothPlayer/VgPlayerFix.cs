b738901ba7c7e348c2125dc52b486da7221c6056
enchart <77974051+enchart@users.noreply.github.com>
2025-11-03 22:57:57 +0500

    Update patches

--- AlternativeSmoothPlayer/VgPlayerFix.cs
+++ AlternativeSmoothPlayer/VgPlayerFix.cs
@@ -4,31 +4,83 @@ namespace AlternativeSmoothPlayer;
 
 public class VgPlayerFix : MonoBehaviour
 {
+    public float rotateSpeed = 40.0f;
+    public float followSpeed = 10.0f;
+    public float edgeSpeed = 25.0f;
+    public bool isFixEnabled;
+
     private VGPlayer _player;
-    private Transform _core;
+    private Transform _rigidbodyTransform;
+
+    private Transform _visual;
+    private DelayTracker _visualDelayTracker;
+
+    private GameObject _wrapper;
+    private MeshRenderer _wrapperMeshRenderer;
+    private MeshFilter _wrapperMeshFilter;
 
     private void Start()
     {
         _player = GetComponent<VGPlayer>();
-        _core = _player.transform.Find("Player");
+        _rigidbodyTransform = _player.Player_Rigidbody.transform;
 
-        Destroy(transform.Find("Player").GetComponent<DelayTracker>());
+        _wrapper = _player.transform.Find("Player-Wrapper").gameObject;
+        _wrapper.transform.localScale = Vector3.one * 0.75f;
+
+        _wrapperMeshRenderer = _wrapper.AddComponent<MeshRenderer>();
+        _wrapperMeshRenderer.material = new Material(Shader.Find("Hidden/Internal-Colored"));
+        _wrapperMeshRenderer.enabled = false;
+
+        _wrapperMeshFilter = _wrapper.AddComponent<MeshFilter>();
+        _wrapperMeshFilter.mesh = _player.transform.Find("Player/core").GetComponent<MeshFilter>().mesh;
+
+        _visual = _player.transform.Find("Player");
+        _visualDelayTracker = _visual.GetComponent<DelayTracker>();
+        _visualDelayTracker.enabled = !isFixEnabled;
     }
 
     private void Update()
     {
-        const float multiplier = 40.0f;
-        
-        var target = _player.Player_Rigidbody.transform;
-        
-        // Time.deltaTime or Time.smoothDeltaTime
-        _core.position = Vector3.Lerp(_core.position, target.position, Time.smoothDeltaTime * multiplier);
-        _core.rotation = Quaternion.Lerp(_core.rotation, Quaternion.Euler(0f, 0f, target.eulerAngles.z), Time.smoothDeltaTime * multiplier);
+        if (Input.GetKeyDown(KeyCode.V))
+        {
+            _wrapperMeshRenderer.enabled = !_wrapperMeshRenderer.enabled;
+            _player.Player_Text.DisplayText($"Render <b>{(_wrapperMeshRenderer.enabled ? "enabled" : "disabled")}</b>", 1f);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            isFixEnabled = !isFixEnabled;
+            _player.Player_Text.DisplayText($"Fix <b>{(isFixEnabled ? "enabled" : "disabled")}</b>", 1f);
+            _visualDelayTracker.enabled = !isFixEnabled;
+        }
+
+        if (isFixEnabled)
+        {
+            var positionDelta = (Vector3)_player.internalVelocity;
+            var rotationDelta = Quaternion.Euler(0f, 0f, _rigidbodyTransform.eulerAngles.z);
+
+            _visual.position += positionDelta * Time.deltaTime;
+            _visual.rotation = Quaternion.Lerp(_visual.rotation, rotationDelta, Time.deltaTime * rotateSpeed);
+        }
     }
 
-    private void FixedUpdate()
+    private void LateUpdate()
     {
-        if (_player.CanMove)
-            _player.Player_Rigidbody.velocity = _player.internalVelocity;
+        if (isFixEnabled)
+        {
+            _visual.position = Vector3.Lerp(_visual.position, _rigidbodyTransform.position, Time.deltaTime * followSpeed);
+
+            var vector = _player.ObjectCamera.WorldToViewportPoint(_visual.position);
+            var edgeOffset = VGPlayer.EDGE_OFFSET;
+            var num = (float)Screen.height / Screen.width * edgeOffset;
+
+            if (vector.x < num || vector.x > 1f - num || vector.y < edgeOffset || vector.y > 1f - edgeOffset)
+            {
+                vector.x = Mathf.Clamp(vector.x, num, 1f - num);
+                vector.y = Mathf.Clamp(vector.y, edgeOffset, 1f - edgeOffset);
+                _visual.position = Vector3.Lerp(_visual.position, _player.ObjectCamera.ViewportToWorldPoint(vector),
+                    Time.deltaTime * edgeSpeed);
+            }
+        }
     }
 }
--- AlternativeSmoothPlayer/VgPlayerPatch.cs
+++ AlternativeSmoothPlayer/VgPlayerPatch.cs
@@ -14,29 +14,6 @@ public static class VgPlayerPatch
         if (__instance.GetComponent<VgPlayerFix>() == null)
             __instance.gameObject.AddComponent<VgPlayerFix>();
 
-        if (__instance.IsPaused)
-        {
-            __instance.Player_Rigidbody.velocity = Vector2.zero;
-            return false;
-        }
-
-        __instance.p_MonoColor = !__instance.CanTakeDamage;
-        __instance.HandleBoost();
-        __instance.HandleLastMove();
-        if (__instance.IsHurt)
-        {
-            __instance.isHurting -= Time.deltaTime;
-        }
-
-        if (__instance.CanMove)
-        {
-            __instance.HandlePlayerInput();
-            __instance.RotatePlayer();
-            // __instance.UpdatePlayerVelocity();
-            return false;
-        }
-
-        __instance.Player_Rigidbody.velocity = Vector2.zero;
-        return false;
+        return true;
     }
 }
